dcdce85b4b8b7b3ec0a3977d1029dd15b4198df7
Alex <alex.averill2013@gmail.com>
2020-03-17 19:18:23 -0600

    Removing sheets example code

--- index.js
+++ index.js
@@ -68,30 +68,6 @@ function getNewToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-function listMajors(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1c6A2jXyykKjhi6PNUUsZ1F3RJbyn77rb1X6AjzaB92o',
-    range: 'Site Information',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
-    if (rows.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
-      rows.map((row) => {
-        console.log(`${row[0]}, ${row[1]}`);
-      });
-    } else {
-      console.log('No data found.');
-    }
-  });
-}
 async function getSheet(auth,pageTitle){
     const sheets = google.sheets({version: 'v4', auth});
     let s = await sheets.spreadsheets.values.get({
